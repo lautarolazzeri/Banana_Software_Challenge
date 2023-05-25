38c9df0ebdc176f039e38e7c5c3b04457da13210
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-05-25 17:39:07 -0300

    api and new UserModel

--- Flutter_Banana_Challenge/lib/constants/constants.dart
+++ Flutter_Banana_Challenge/lib/constants/constants.dart
@@ -3,3 +3,4 @@ import "package:flutter/material.dart";
 //all constants are exported here for easy access from anywhere
 const appColor = Color(0xFF9E007E);
 const white = Colors.white;
+const api = 'https://dummyjson.com';
---
+++ Flutter_Banana_Challenge/lib/models/user_model.dart
@@ -0,0 +1,53 @@
+// To parse this JSON data, do
+//
+//     final productModel = productModelFromMap(jsonString);
+
+import 'dart:convert';
+
+ProductModel productModelFromMap(String str) => ProductModel.fromMap(json.decode(str));
+
+String productModelToMap(ProductModel data) => json.encode(data.toMap());
+
+class ProductModel {
+  final int id;
+  final String username;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String gender;
+  final String image;
+  final String token;
+
+  ProductModel({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.gender,
+    required this.image,
+    required this.token,
+  });
+
+  factory ProductModel.fromMap(Map<String, dynamic> json) => ProductModel(
+        id: json["id"],
+        username: json["username"],
+        email: json["email"],
+        firstName: json["firstName"],
+        lastName: json["lastName"],
+        gender: json["gender"],
+        image: json["image"],
+        token: json["token"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "username": username,
+        "email": email,
+        "firstName": firstName,
+        "lastName": lastName,
+        "gender": gender,
+        "image": image,
+        "token": token,
+      };
+}
