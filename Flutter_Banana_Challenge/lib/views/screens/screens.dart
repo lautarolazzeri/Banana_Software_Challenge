30b0ed6c0dd4745a071a4f8cc97a6837aa260d45
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-05-26 17:36:52 -0300

    asc lines

--- Flutter_Banana_Challenge/lib/views/screens/screens.dart
+++ Flutter_Banana_Challenge/lib/views/screens/screens.dart
@@ -1,8 +1,6 @@
 //all screens are exported here
 
-export 'package:app/views/screens/product_info_screen.dart';
-
 export 'package:app/views/screens/home_screen.dart';
-
 export 'package:app/views/screens/login_screen.dart';
+export 'package:app/views/screens/product_info_screen.dart';
 
