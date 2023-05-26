1c13d99e9ed5ca36bb4c7a8d856f763465a5b96a
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-05-26 17:46:13 -0300

    exported file

--- Flutter_Banana_Challenge/lib/widgets/widgets.dart
+++ Flutter_Banana_Challenge/lib/widgets/widgets.dart
@@ -1,3 +1,4 @@
+//all widgets are exported here for easy access
 export 'package:app/widgets/search/search_delegate.dart';
 export 'package:app/widgets/homeScreen/product_widget.dart';
 export 'package:app/widgets/homeScreen/search_widget.dart';
